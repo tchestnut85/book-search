4979411be782c45189481e96c20bbd570f8af5b6
tchestnut85 <tchestnut85@gmail.com>
2021-01-16 17:09:03 -0500

    added client side graphql mutations for creating a user, logging in, saving and remoiving books.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation login($username:String!, $password: String!) {
+        login(username:$username, password:$password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username:String!, $email:String!, $password: String!){
+        addUser(username: $username, email:$email, password:$password) {
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: ID!, $image: String!, $link: String!) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image:$image, link: $link) {
+            username
+            bookCount
+            savedBooks {
+                title
+                authors
+                bookId
+                description
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            user {
+                _id
+                username
+                bookCount
+                savedBooks {
+                    title
+                    authors
+                    bookId
+                    description
+                }
+            }
+        }
+    }
+`;
